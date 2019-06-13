835555196b06d6649f38c2a1aa3d9f03f71a16ab
Yuhei Tomita <tomita.yuhei@moneyforward.co.jp>
2019-06-13 17:36:37 +0900

    位置情報を送るボタンを設置

--- app.rb
+++ app.rb
@@ -11,35 +11,17 @@ end
 def template
 {
     "type": "template",
-    "altText": "This is a buttons template",
+    "altText": "OL検索中",
     "template": {
         "type": "buttons",
-        "thumbnailImageUrl": "https://example.com/bot/images/image.jpg",
-        "imageAspectRatio": "rectangle",
-        "imageSize": "cover",
-        "imageBackgroundColor": "#FFFFFF",
-        "title": "Menu",
-        "text": "Please select",
-        "defaultAction": {
-            "type": "uri",
-            "label": "View detail",
-            "uri": "http://example.com/page/123"
+        "title": "最寄りのOLを検索する",
+        "text": "現在の位置を送信しますか？",
         },
         "actions": [
             {
-                "type": "postback",
-                "label": "Buy",
-                "data": "action=buy&itemid=123"
-            },
-            {
-                "type": "postback",
-                "label": "Add to cart",
-                "data": "action=add&itemid=123"
-            },
-            {
                 "type": "uri",
-                "label": "View detail",
-                "uri": "http://example.com/page/123"
+                "label": "位置を送る",
+                "uri": "line://nv/location"
             }
         ]
     }
